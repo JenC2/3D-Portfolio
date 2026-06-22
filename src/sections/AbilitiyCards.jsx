a0d12c130281923a45ce14c4e89c9014f18ce770
jennychen <jenny.chenchou@gmail.com>
2026-06-22 22:39:33 +0800

    add text-body to global style

--- src/sections/AbilitiyCards.jsx
+++ src/sections/AbilitiyCards.jsx
@@ -5,21 +5,23 @@ const AbilitiyCards = () => {
   return (
     <div className="w-full section-padding">
       <div className="w-full h-full md:px-20 px-5">
-      <TitleHeader title="My Qualities" />
-      <div className="mx-auto grid-4-cols mt-32">
-        {abilities.map(({ imgPath, title, desc }) => (
-          <div
-            key={title}
-            className="card-border rounded-xl p-8 flex flex-col gap-4 text-center"
-          >
-            <div className="flex items-center justify-center p-5">
-              <img src={imgPath} alt={title} className="rounded-xl" />
+        <TitleHeader title="My Qualities" />
+        <div className="mx-auto grid-4-cols mt-32">
+          {abilities.map(({ imgPath, title, desc }) => (
+            <div
+              key={title}
+              className="card-border rounded-xl p-8 flex flex-col gap-4 text-center"
+            >
+              <div className="flex items-center justify-center p-5">
+                <img src={imgPath} alt={title} className="rounded-xl" />
+              </div>
+              <h3 className="text-2xl md:text-2xl lg:text-3xl font-semibold">
+                {title}
+              </h3>
+              <p className="text-white-50 text-body">{desc}</p>
             </div>
-            <h3 className="text-white text-2xl font-semibold">{title}</h3>
-            <p className="text-white-50 text-lg ">{desc}</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -81,14 +81,18 @@ const ExperienceSection = () => {
                     <div className="w-1/6 p-8"></div>
                     <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
                       <div>
-                        <h1 className="font-semibold text-3xl">{card.title}</h1>
-                        <p className="my-5 text-white-50">{card.date}</p>
-                        <p className="text-[#839cb5] italic">
+                        <h1 className="font-semibold text-xl md:text-2xl lg:text-4xl">
+                          {card.title}
+                        </h1>
+                        <p className="my-5 text-white-50 text-body">
+                          {card.date}
+                        </p>
+                        <p className="text-[#839cb5] italic text-body">
                           Responsibilities
                         </p>
                         <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
                           {card.responsibilities.map((responsibility) => (
-                            <li key={responsibility} className="text-lg">
+                            <li key={responsibility} className="text-body">
                               {responsibility}
                             </li>
                           ))}
